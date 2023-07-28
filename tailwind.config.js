d39ef50b25af107c50b1771338ba2aef61ac74d7
maxall41 <maxall4@protonmail.com>
2023-07-27 20:10:26 -0700

    X

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     'index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "title": ["BerkelyMonoBold"]
+      }
+    },
   },
   plugins: [],
 }
